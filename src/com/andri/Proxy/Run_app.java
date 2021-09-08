f6c8eb86c021428ce639141925c6055a7dd5670f
tri andri <triandripp@gmail.com>
2021-09-09 01:09:05 +0700

    --init Project

---
+++ src/com/andri/Proxy/Run_app.java
@@ -0,0 +1,13 @@
+package com.andri.Proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Run_app {
+	public static void main(String[] args) {
+		  SpringApplication.run(Run_app.class, args);
+	        System.out.println("Spring boot application running with starter parent");
+	}
+
+}
